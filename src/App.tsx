ea32aea169401e33a2fed5589ee8c35722397d2a
Kotov <kotov.smtp@gmail.com>
2022-05-02 19:24:13 +0300

    ⚙️ react router dom added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import ScrollToTop from "./components/ScrollToTop";
+
+import Main from "./components/Main";
+import Play from "./components/Play";
+import NotFound from "./components/NotFound";
+
 const App: React.FC = () => (
-  <div>
-    <h1 className='text-2xl font-bold'>Mafia Helper</h1>
-  </div>
+  <Router>
+    <ScrollToTop />
+
+    <Routes>
+      <Route path='/' element={<Main />} />
+      <Route path='play' element={<Play />} />
+      <Route path='*' element={<NotFound />} />
+    </Routes>
+  </Router>
 );
 
 export default App;
