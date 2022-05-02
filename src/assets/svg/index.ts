ee2cabc94a800e110dc46fd3287e8cec01fda9ea
Kotov <kotov.smtp@gmail.com>
2022-05-02 19:24:43 +0300

    🖼 assets added

---
+++ src/assets/svg/index.ts
@@ -0,0 +1,5 @@
+import { ReactComponent as RoseLogo } from "./rose-logo.svg";
+import { ReactComponent as GithubIcon } from "./github.svg";
+import { ReactComponent as TelegramIcon } from "./telegram.svg";
+
+export { RoseLogo, GithubIcon, TelegramIcon };
